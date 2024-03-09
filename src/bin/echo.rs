67201cd423679f8d3ab1223af6617def2ef3966c
ahmetemregurdal <ahmetemregurdal2008@gmail.com>
2024-03-09 12:19:48 +0300

    Seperated a long piece of code to it's own function

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, ArgAction};
+use clap::{ArgAction, Parser};
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -9,6 +9,47 @@ struct Args {
     enable_backslash: bool,
     #[arg(short = 'E', long, help = "Disable interpretation of backslash escapes(default)", action = ArgAction::SetTrue)]
     disable_backslash: bool,
+}
+
+fn parse_backslash(a: String) -> String {
+    let mut new_message: String = "".to_string();
+    let original_text_vec: Vec<char> = a.chars().collect();
+    let mut skip = false;
+    for i in 0..(original_text_vec.len() - 1) {
+        if skip {
+            skip = false;
+            continue;
+        } else if i == original_text_vec.len() - 1 {
+            new_message.push(original_text_vec[i]);
+        } else if original_text_vec[i] != '\\' {
+            new_message.push(original_text_vec[i]);
+        } else {
+            skip = true;
+            if original_text_vec[i + 1] == 'n' {
+                new_message.push('\n');
+            } else if original_text_vec[i + 1] == '\\' {
+                new_message.push('\\');
+            } else if original_text_vec[i + 1] == 'b' {
+                new_message.push('\x08');
+            } else if original_text_vec[i + 1] == 'a' {
+                new_message.push('\x07');
+            } else if original_text_vec[i + 1] == 'c' {
+                break;
+            } else if original_text_vec[i + 1] == 'e' {
+                new_message.push('\x1b');
+            } else if original_text_vec[i + 1] == 'f' {
+                new_message.push('\x0c');
+            } else if original_text_vec[i + 1] == 'r' {
+                new_message.push('\r');
+            } else if original_text_vec[i + 1] == 't' {
+                new_message.push('\t');
+            } else if original_text_vec[i + 1] == 'v' {
+                new_message.push('\x0b');
+            }
+            // TODO: Support gor \0NNN and \xHH
+        }
+    }
+    return new_message;
 }
 
 fn main() {
@@ -23,47 +64,10 @@ fn main() {
             print!("{}", cli.message.unwrap());
         }
     } else {
-        let mut new_message: String = "".to_string();
-        let original_text_vec: Vec<char> = cli.message.unwrap().chars().collect();
-        let mut skip = false;
-        for i in 0..(original_text_vec.len() - 1) {
-            if skip {
-                skip = false;
-                continue;
-            } else if i == original_text_vec.len() - 1 {
-                new_message.push(original_text_vec[i]);
-            } else if original_text_vec[i] != '\\' {
-                new_message.push(original_text_vec[i]);
-            } else {
-                skip = true;
-                if original_text_vec[i + 1] == 'n' {
-                    new_message.push('\n');
-                } else if original_text_vec[i + 1] == '\\' {
-                    new_message.push('\\');
-                } else if original_text_vec[i + 1] == 'b' {
-                    new_message.push('\x08');
-                } else if original_text_vec[i + 1] == 'a' {
-                    new_message.push('\x07');
-                } else if original_text_vec[i + 1] == 'c' {
-                    break;
-                } else if original_text_vec[i + 1] == 'e' {
-                    new_message.push('\x1b');
-                } else if original_text_vec[i + 1] == 'f' {
-                    new_message.push('\x0c');
-                } else if original_text_vec[i + 1] == 'r' {
-                    new_message.push('\r');
-                } else if original_text_vec[i + 1] == 't' {
-                    new_message.push('\t');
-                } else if original_text_vec[i + 1] == 'v' {
-                    new_message.push('\x0b');
-                }
-                // TODO: Support gor \0NNN and \xHH
-            }
-        }
         if cli.no_newline {
-            print!("{new_message}");
+            print!("{}", parse_backslash(cli.message.unwrap()));
         } else {
-            println!("{new_message}");
+            println!("{}", parse_backslash(cli.message.unwrap()));
         }
     }
 }
